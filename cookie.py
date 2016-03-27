e97ca98574b7cbb1b3d4b03e8fc75a281c04ceb2
Ryan Barrett <git@ryanb.org>
2016-03-26 22:41:17 -0700

    port to new instagram scraping in granary's get_activities()

--- cookie.py
+++ cookie.py
@@ -7,9 +7,10 @@ import urllib
 import urllib2
 
 import appengine_config
-from granary import atom, instagram
-import webapp2
+from granary import atom, instagram, source
 from oauth_dropins.webutil import util
+import webapp2
+from webob import exc
 
 
 class CookieHandler(webapp2.RequestHandler):
@@ -17,29 +18,27 @@ class CookieHandler(webapp2.RequestHandler):
   def get(self):
     cookie = 'sessionid=%s' % urllib.quote(util.get_required_param(self, 'sessionid'))
     logging.info('Fetching with Cookie: %s', cookie)
-    resp = urllib2.urlopen(urllib2.Request(
-      'https://www.instagram.com/',
-      headers={'Cookie': cookie}),
-      timeout=60)
-    body = resp.read().decode('utf-8')
-    logging.info('Response: %s', resp.getcode())
-
-    if resp.getcode() != 200 or 'not-logged-in' in body:
-      return self.abort(401, "Couldn't log into Instagram with cookie %s" % cookie)
 
     ig = instagram.Instagram()
-    activities, viewer = ig.html_to_activities(body)
-    if viewer:
+    try:
+      resp = ig.get_activities_response(group_id=source.FRIENDS, scrape=True,
+                                        cookie=cookie)
+    except Exception as e:
+      self.response.status, self.response.text = util.interpret_http_exception(e)
+      return
+
+    actor = resp.get('actor')
+    if actor:
       logging.info('Logged in as %s (%s)',
-                   viewer.get('username'), viewer.get('displayName'))
+                   actor.get('username'), actor.get('displayName'))
     else:
       logging.warning("Couldn't determine Instagram user!")
 
-    title = 'instagram-atom feed for %s' % ig.actor_name(viewer)
+    title = 'instagram-atom feed for %s' % ig.actor_name(actor)
     self.response.headers['Content-Type'] = 'application/atom+xml'
     self.response.out.write(atom.activities_to_atom(
-      activities, viewer, title=title, host_url=self.request.host_url + '/',
-      request_url=self.request.path_url))
+      resp.get('items', []), actor, title=title, host_url=self.request.host_url + '/',
+      request_url=self.request.path_url, xml_base='https://www.instagram.com/'))
 
 
 application = webapp2.WSGIApplication(
